52d89fe2ef52140304d5e26b0254a36d4ae6371b
Artyom Labazov <123321artyom@gmail.com>
2016-11-12 16:07:23 +0300

    kernel/sched: Fix derp

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -967,19 +967,10 @@ static inline unsigned int do_avg_nr_running(struct rq *rq)
 
 static inline void inc_nr_running(struct rq *rq)
 {
-<<<<<<< HEAD
-	rq->nr_running++;
-
-	if (rq->nr_running >= 2) {
-		if (!rq->rd->overload)
-			rq->rd->overload = true;
-	}
-=======
 #ifdef CONFIG_INTELLI_PLUG
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
 
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 #ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
@@ -989,24 +980,24 @@ static inline void inc_nr_running(struct rq *rq)
 #ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_end(&nr_stats->ave_seqcnt);
 #endif
->>>>>>> 3fc4e80... intelli_plug: refactor stats calculation code to be less intrusive
+
+	if (rq->nr_running >= 2) {
+		if (!rq->rd->overload)
+			rq->rd->overload = true;
+	}
 }
 
 static inline void dec_nr_running(struct rq *rq)
 {
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_INTELLI_PLUG
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
 
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 #ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
 	nr_stats->nr_last_stamp = rq->clock_task;
 #endif
->>>>>>> 3fc4e80... intelli_plug: refactor stats calculation code to be less intrusive
 	rq->nr_running--;
 #ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_end(&nr_stats->ave_seqcnt);
